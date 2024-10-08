70655ab417c89da5a6d4bbdbd7fdf31da892e85b
Munira12345 <munirangomo@gmail.com>
2024-10-08 09:14:40 +0300

    commits

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.apollographql.apollo3") version "3.8.1" apply false
+    id("com.apollographql.apollo3") version "3.7.2" apply false
 }
  task<Delete>("clean") {
         delete(rootProject.buildDir)
